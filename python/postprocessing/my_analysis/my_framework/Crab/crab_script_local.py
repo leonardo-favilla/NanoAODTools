4509591aead4a8b5d0c0a0750a3eb42da744eb16
Leonardo Favilla <leonardo.favilla18@gmail.com>
2023-06-06 15:09:09 +0200

    run tDM only

--- python/postprocessing/my_analysis/my_framework/Crab/crab_script_local.py
+++ python/postprocessing/my_analysis/my_framework/Crab/crab_script_local.py
@@ -20,8 +20,8 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopevaluate imp
 ########### Create arguments to insert from shell ###########
 from argparse import ArgumentParser
 parser      = ArgumentParser()
-parser.add_argument("-file",        dest="file",        required=True, type=str,  help="skim file to run")
-parser.add_argument("-files_path",  dest="files_path",  required=True, type=str,  help="path where skim files are saved (after nanoTopCandidate)")
+parser.add_argument("-file",        dest="file",        required=True, type=str,  help="file to run")
+parser.add_argument("-files_path",  dest="files_path",  required=True, type=str,  help="path where skim files are saved")
 parser.add_argument("-save_path",   dest="save_path",   required=True, type=str,  help="path where to save files")
 parser.add_argument("-nev",         dest="nev",         required=True, type=int,  help="number of events to use")
 options     = parser.parse_args()
@@ -32,10 +32,17 @@ save_path   = options.save_path           # Where to save skim
 nev         = options.nev
 
 ############ DATASET PROCESSING ############
-
+if ('Data' in sample.label):
+  isMC   = False
+  # presel = "Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_eeBadScFilter "
+else:
+  isMC   = True
 # print("Before PostProcessor")
 path_to_file  = f"{files_path}/{file}"
-modules       = [nanoTopevaluate()]
+if isMC:
+  modules = "MCweight_writer(), PreSkimSetup(), InitSkim(), GenPart_MomFirstCp(flavour='-5,-4,-3,-2,-1,1,2,3,4,5,6,-6,24,-24'), nanoprepro(), nanoTopcand("+str(isMC)+"), W_Top_Tagger(), Re_Bo_Tagger(), Merge_Tagger(), nanoTopevaluate()"
+else:
+  modules = "MCweight_writer(), PreSkimSetup(), InitSkim(), nanoTopcand("+str(isMC)+"), W_Top_Tagger(), Re_Bo_Tagger(), Merge_Tagger(), nanoTopevaluate()"
 p             = PostProcessor(outputDir=".", inputFiles=[path_to_file], cut="", modules=modules, outputbranchsel=os.path.abspath("./keep_and_drop.txt"), provenance=True, maxEntries=nev, fwkJobReport=False)                                                             
 p.run()
 print("DONE")
--- python/postprocessing/my_analysis/my_framework/Crab/setup_crab_local.py
+++ python/postprocessing/my_analysis/my_framework/Crab/setup_crab_local.py
@@ -12,13 +12,18 @@ if not os.path.exists(save_path):
     os.mkdir(save_path)
 nev             = 30000
 # Samples #
-datasets    = ["tDM_mPhi1000_mChi1", "QCD_HT1000to1500","QCD_HT1500to2000", "QCD_HT2000toInf", "TT_Mtt_700to1000", "TT_Mtt_1000toInf"]
-files       = {datasets[0]: "tDM_Mphi1000_2018_Skim.root",
-               datasets[1]: "QCD_HT1000to1500_2018_Skim.root",
-               datasets[2]: "QCD_HT1500to2000_2018_Skim.root",
-               datasets[3]: "QCD_HT2000toInf_2018_Skim.root",
-               datasets[4]: "TT_Mtt_700to1000_2018_Skim.root",
-               datasets[5]: "TT_Mtt_1000toInf_2018_Skim.root"
+# datasets    = ["tDM_mPhi1000_mChi1", "QCD_HT1000to1500","QCD_HT1500to2000", "QCD_HT2000toInf", "TT_Mtt_700to1000", "TT_Mtt_1000toInf"]
+# files       = {datasets[0]: "tDM_Mphi1000_2018_Skim.root",
+#                datasets[1]: "QCD_HT1000to1500_2018_Skim.root",
+#                datasets[2]: "QCD_HT1500to2000_2018_Skim.root",
+#                datasets[3]: "QCD_HT2000toInf_2018_Skim.root",
+#                datasets[4]: "TT_Mtt_700to1000_2018_Skim.root",
+#                datasets[5]: "TT_Mtt_1000toInf_2018_Skim.root"
+#                }
+datasets    = ["tDM_mPhi50_mChi1", "tDM_mPhi500_mChi1", "tDM_mPhi1000_mChi1"]
+files       = {datasets[0]: "tDM_Mphi50_2018_Skim.root",
+               datasets[1]: "tDM_mPhi500_mChi1_Skim.root",
+               datasets[2]: "tDM_mPhi1000_mChi1_Skim.root"
                }
 
 single_skim     = False
