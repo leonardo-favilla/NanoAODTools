6814b921467f1a589ac2acba361a360eeb444b59
Leonardo Favilla <leonardo.favilla18@gmail.com>
2023-06-13 14:44:57 +0200

    added counts histograms and graphs

--- python/postprocessing/my_analysis/my_framework/Utilities/FillHistos.py
+++ python/postprocessing/my_analysis/my_framework/Utilities/FillHistos.py
@@ -5,8 +5,11 @@ import mplhep as hep
 hep.style.use(hep.style.CMS)
 import coffea
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+import numpy as np
+from array import array
 
-
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
 ######### Create arguments to insert from shell #########
@@ -30,67 +33,188 @@ rhistos_filename            = options.rhistos_filename
 
 ######### Take "Events" TTree #########
 tree                = NanoEventsFactory.from_root(f"{path_to_rfile}", schemaclass=NanoAODSchema.v6).events()
+num_entries         = len(tree)
+### Select which histograms must be filled or not ###
+do_CountsVsScore                = False
+do_EfficiencyVsScore            = False
+do_CountsOverThrVsScoreThrs     = False
+do_EfficiencyOverThrVsScoreThrs = False
+
+
+
 ### OutHistos Root File ###
+PlotsRFilePath  = f"{path_to_graphics_folder}/{rhistos_filename}"
 if save_graphics:
     if not os.path.exists(path_to_graphics_folder):
         os.makedirs(path_to_graphics_folder)
-    PlotsRFile  = ROOT.TFile(f"{path_to_graphics_folder}/{rhistos_filename}", "RECREATE")
+    if not os.path.exists(PlotsRFilePath): 
+        PlotsRFile  = ROOT.TFile(PlotsRFilePath, "RECREATE")
+    else:
+        PlotsRFile  = ROOT.TFile(PlotsRFilePath, "UPDATE")
 
 pt_flags        = ["Low", "High"]
 truths          = [True, False]
 
 
 ### Store TopCandidates in dictionary ###
-TopDict         = {"LowFalse" : tree.TopLowPt[(tree.TopLowPt.truth==0)],
-                   "LowTrue"  : tree.TopLowPt[(tree.TopLowPt.truth==1)],
-                   "HighFalse": tree.TopHighPt[(tree.TopHighPt.truth==0)],
-                   "HighTrue" : tree.TopHighPt[(tree.TopHighPt.truth==1)]
-                   }
+if num_entries:
+    TopDict         = {"LowFalse" : tree.TopLowPt[(tree.TopLowPt.truth==0)],
+                       "LowTrue"  : tree.TopLowPt[(tree.TopLowPt.truth==1)],
+                       "HighFalse": tree.TopHighPt[(tree.TopHighPt.truth==0)],
+                       "HighTrue" : tree.TopHighPt[(tree.TopHighPt.truth==1)]
+                       }
 
+
+def TopsOverThr(tree, pt_flag="Low", truth=0, thr=0):
+    if pt_flag=="Low":
+        TopsOverThr    = tree.TopLowPt[(tree.TopLowPt.truth==truth) * (tree.TopLowPt.scoreDNN>=thr)]
+    elif pt_flag=="High":
+        TopsOverThr    = tree.TopHighPt[(tree.TopHighPt.truth==truth) * (tree.TopHighPt.score2>=thr)]
+    return TopsOverThr
 
 ################## HISTOGRAMS ##################
+"""
+Counts vs. Score using:
+1. TopLowPt_pt in [0, 1000]GeV
+2. TopHighPt_pt in [50, inf]GeV
+"""
+if do_CountsVsScore:
+    nbins      = 20
+    xmin       = 0
+    xmax       = 1
+    for pt_flag in pt_flags:
+        for truth in truths:
+            scores     = []
+            if num_entries:
+                if pt_flag=="Low":
+                    scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].scoreDNN)
+                elif pt_flag=="High":
+                    scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].score2)
+                    
+            # Create and Fill TH1F 
+            histoName  = f"Top{pt_flag}Pt_CountsVsScore_{truth}"
+            histoTitle = f"Top{pt_flag}Pt_CountsVsScore_{truth}"
+            histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+            for score in scores:
+                histo.Fill(score)
+                
+            histo.GetXaxis().SetTitle("Score")
+            histo.GetYaxis().SetTitle("Counts")
+            histo.SetFillStyle(3001)
+            histo.SetLineWidth(2)
+            histo.SetLineColor(ROOT.kBlue)
+            # histo.Draw()
+            # Save histo to .root file 
+            if save_graphics:
+                histo.Write()
+            
+            
 """
 Efficiency(Counts/totCounts) vs. Score using:
 1. TopLowPt_pt in [0, 1000]GeV
 2. TopHighPt_pt in [50, inf]GeV
 """
-nbins      = 20
-xmin       = 0
-xmax       = 1
-for pt_flag in pt_flags:
-    for truth in truths:
-        histoName  = f"Top{pt_flag}Pt_Score_{truth}"
-        histoTitle = f"Top{pt_flag}Pt_Score_{truth}"
-        histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
-        if pt_flag=="Low":
-            scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].scoreDNN)
-        elif pt_flag=="High":
-            scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].score2)
-        
-        for score in scores:
-            histo.Fill(score)
-            
-        # Normalize to integral to obtain efficiency
-        if histo.Integral(0, nbins+1):
-            histo.Scale(1/histo.Integral(0, nbins+1))
-        else:
-            histo.Scale(1)
-        histo.GetXaxis().SetTitle("Score")
-        histo.GetYaxis().SetTitle("Efficiency")
-        histo.SetFillStyle(3001)
-        histo.SetLineWidth(2)
-        histo.SetLineColor(ROOT.kBlue)
-        # histo.Draw()
-        # Save histo to .root file 
-        if save_graphics:
-            histo.Write()
+if do_EfficiencyVsScore:
+    nbins      = 20
+    xmin       = 0
+    xmax       = 1
+    for pt_flag in pt_flags:
+        for truth in truths:
+            scores     = []
+            if num_entries:
+                if pt_flag=="Low":
+                    scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].scoreDNN)
+                elif pt_flag=="High":
+                    scores = ak.flatten(TopDict[f"{pt_flag}{truth}"].score2)
+                    
+            # Create and Fill TH1F 
+            histoName  = f"Top{pt_flag}Pt_EfficiencyVsScore_{truth}"
+            histoTitle = f"Top{pt_flag}Pt_EfficiencyVsScore_{truth}"
+            histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+            for score in scores:
+                histo.Fill(score)
+                
+            # Normalize to integral to obtain efficiency
+            if histo.Integral(0, nbins+1):
+                histo.Scale(1/histo.Integral(0, nbins+1))
+            else:
+                histo.Scale(1)
+            histo.GetXaxis().SetTitle("Score")
+            histo.GetYaxis().SetTitle("Efficiency")
+            histo.SetFillStyle(3001)
+            histo.SetLineWidth(2)
+            histo.SetLineColor(ROOT.kBlue)
+            # histo.Draw()
+            # Save histo to .root file 
+            if save_graphics:
+                histo.Write()
 
 
 """
-Efficiency(Counts/totCounts) vs. ScoreThreshold using:
+TopOverThr vs. Score Thresholds using:
 1. TopLowPt_pt in [0, 1000]GeV
 2. TopHighPt_pt in [50, inf]GeV
 """
-
-
-
+if do_CountsOverThrVsScoreThrs:
+    scoreThrs  = np.arange(0, 1.1, 0.1)
+    for pt_flag in pt_flags:
+        for truth in truths:
+            counts = []
+            for thr in scoreThrs:
+                if num_entries:
+                    Tops_Over_Thr  = TopsOverThr(tree=tree, pt_flag=pt_flag, truth=truth, thr=thr)
+                    count          = ak.sum(ak.num(Tops_Over_Thr))
+                counts.append(count)
+                
+            # Create and Fill TGraph 
+            graph      = ROOT.TGraph(len(counts), array("d", scoreThrs), array("d", counts))
+            graphName  = f"Top{pt_flag}Pt_CountsOverThrVsScoreThrs_{truth}"
+            graphTitle = f"Top{pt_flag}Pt_CountsOverThrVsScoreThrs_{truth}"
+            graph.SetName(graphName)
+            graph.SetTitle(graphTitle)
+            graph.GetXaxis().SetTitle("Score Thrs")
+            graph.GetYaxis().SetTitle("Top Over Thr")
+            graph.SetLineColor(ROOT.kBlue)
+            graph.SetLineStyle(2)
+            graph.SetLineWidth(2)
+            graph.SetMarkerStyle(ROOT.kFullCircle)
+            graph.SetMarkerSize(1.5)
+            if save_graphics:
+                graph.Write()
+                
+                
+"""
+EfficiencyOverThr(TopOverThr/nTop) vs. Score Thresholds using:
+1. TopLowPt_pt in [0, 1000]GeV
+2. TopHighPt_pt in [50, inf]GeV
+"""
+if do_EfficiencyOverThrVsScoreThrs:
+    scoreThrs  = np.arange(0, 1.1, 0.1)
+    for pt_flag in pt_flags:
+        for truth in truths:
+            efficiencies = []
+            for thr in scoreThrs:
+                efficiency         = 0
+                if num_entries:
+                    ntop           = ak.sum(ak.num(TopDict[f"{pt_flag}{truth}"]))
+                    Tops_Over_Thr  = TopsOverThr(tree=tree, pt_flag=pt_flag, truth=truth, thr=thr)
+                    count          = ak.sum(ak.num(Tops_Over_Thr))
+                    if ntop:
+                        efficiency = count / ntop
+                efficiencies.append(efficiency)
+                
+            # Create and Fill TGraph 
+            graph      = ROOT.TGraph(len(efficiencies), array("d", scoreThrs), array("d", efficiencies))
+            graphName  = f"Top{pt_flag}Pt_ScoreThrs_{truth}"
+            graphTitle = f"Top{pt_flag}Pt_ScoreThrs_{truth}"
+            graph.SetName(graphName)
+            graph.SetTitle(graphTitle)
+            graph.GetXaxis().SetTitle("Score Thrs")
+            graph.GetYaxis().SetTitle("Efficiency Over Thr")
+            graph.SetLineColor(ROOT.kBlue)
+            graph.SetLineStyle(2)
+            graph.SetLineWidth(2)
+            graph.SetMarkerStyle(ROOT.kFullCircle)
+            graph.SetMarkerSize(1.5)
+            if save_graphics:
+                graph.Write()
--- python/postprocessing/my_analysis/my_framework/Utilities/LaunchFillHistos.py
+++ python/postprocessing/my_analysis/my_framework/Utilities/LaunchFillHistos.py
@@ -1,52 +1,23 @@
 import os
 import ROOT
 import json
+from tqdm import tqdm
 # Samples
 from PhysicsTools.NanoAODTools.postprocessing.samples.Samples import *
 
 
-###### Save utilities to a json file ######
+###### Save utilities from a json file to dictionary ######
 path_to_json  = "/afs/cern.ch/user/l/lfavilla/CMSSW_12_6_0/src/PhysicsTools/NanoAODTools/python/postprocessing/my_analysis/my_framework/Utilities"
 json_filename = "utilities.json"
-with open(f"{path_to_json}/{json_filename}", 'r') as f:
+with open(f"{path_to_json}/{json_filename}", "r") as f:
     utilities = json.load(f)
 
 
 
-
 save_graphics       = True
-for label in utilities.keys():
+for label in tqdm(utilities.keys()):
     for c in utilities[label].keys():
         print(f"Making Histograms of component:\t{c}")
         for path_to_rfile, path_to_rHisto in zip(utilities[label][c]["rFiles"], utilities[label][c]["rHistos"]):
-            print(f"\tRunning:\t{path_to_rfile}\tSaving histos to:\t{path_to_rHisto}")
+            print(f"\tRunning:\t{path_to_rfile}\n\tSaving histos to:\t{path_to_rHisto}")
             os.system(f"python3 FillHistos.py -dataset {c} -path_to_rfile {path_to_rfile} -save_graphics {save_graphics} -path_to_graphics_folder {'/'.join(path_to_rHisto.split('/')[:-1])} -rhistos_filename {path_to_rHisto.split('/')[-1]}")
-
-
-# dataset             = "tDM_Mphi50_2018"
-# dataset             = "TprimeBToTZ_M1200_2018"
-# dataset             = "QCD_HT700to1000_2018"
-# dataset             = "ZJetsToNuNu_HT800To1200_2018"
-
-
-
-# path_to_rfile       = "/eos/user/l/lfavilla/my_framework/Skim_Folder/tDM_Mphi50_2018.root"
-# path_to_rfile       = "root://cms-xrd-global.cern.ch//store/user/lfavilla/DM_v0/TprimeBToTZ_M-1200_LH_TuneCP5_PSweights_13TeV-madgraph_pythia8/TprimeBToTZ_M1200_2018/230608_081511/0000/tree_hadd_3.root"
-# path_to_rfile       = "root://cms-xrd-global.cern.ch//store/user/lfavilla/DM_v0/QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraph-pythia8/QCD_HT700to1000_2018/230607_105057/0000/tree_hadd_4.root"
-# path_to_rfile       = "root://cms-xrd-global.cern.ch//store/user/lfavilla/DM_v0/ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/ZJetsToNuNu_HT800To1200_2018/230606_104619/0000/tree_hadd_5.root"
-# nev                 = 1000
-# path_to_graphics    = "/eos/user/l/lfavilla/my_framework/Plots"
-
-
-
-# os.system(f"python3 FillHistos.py -dataset {dataset} -path_to_rfile {path_to_rfile} -save_graphics {save_graphics} -path_to_graphics {path_to_graphics}")
-
-
-
-
-
-
-
-
-
-
