73b708998b8f4a071622c319076a73bf8a248ed8
Leonardo Favilla <leonardo.favilla18@gmail.com>
2023-06-21 11:55:30 +0200

    Added counts and efficiency in pt_bins

--- python/postprocessing/my_analysis/my_framework/Utilities/FillHistos.py
+++ python/postprocessing/my_analysis/my_framework/Utilities/FillHistos.py
@@ -37,17 +37,21 @@ rhistos_filename            = options.rhistos_filename
 tree                = NanoEventsFactory.from_root(f"{path_to_rfile}", schemaclass=NanoAODSchema.v6).events()
 num_entries         = len(tree)
 ### Select which histograms must be filled or not ###
-do_ALL = True
+do_ALL = False
 if do_ALL:
     do_CountsVsScore                = True
     do_EfficiencyVsScore            = True
     do_CountsOverThrVsScoreThrs     = True
     do_EfficiencyOverThrVsScoreThrs = True
+    do_CountsVsScore_ptSlices       = True
+    do_EfficiencyVsScore_ptSlices   = True
 else:
     do_CountsVsScore                = False
     do_EfficiencyVsScore            = False
     do_CountsOverThrVsScoreThrs     = False
     do_EfficiencyOverThrVsScoreThrs = False
+    do_CountsVsScore_ptSlices       = False
+    do_EfficiencyVsScore_ptSlices   = True
 
 
 
@@ -80,6 +84,22 @@ def TopsOverThr(tree, pt_flag="Low", truth=0, thr=0):
     elif pt_flag=="High":
         TopsOverThr    = tree.TopHighPt[(tree.TopHighPt.truth==truth) * (tree.TopHighPt.score2>=thr)]
     return TopsOverThr
+
+
+def slices(pt_start=0, pt_stop=1000, n_slices=10):
+    slices = np.linspace(pt_start, pt_stop, n_slices+1)
+    slices = [(slices[i], slices[i+1]) for i in range(n_slices)]
+    return slices
+
+
+def TopsInSlice(tree, pt_flag="Low", truth=0, pt_low=0, pt_high=1000):
+    if pt_flag=="Low":
+        TopsInSlice = tree.TopLowPt[(tree.TopLowPt.truth==truth) * (tree.TopLowPt.pt>pt_low) * (tree.TopLowPt.pt<pt_high)]
+    elif pt_flag=="High":
+        TopsInSlice = tree.TopHighPt[(tree.TopHighPt.truth==truth) * (tree.TopHighPt.pt>pt_low) * (tree.TopHighPt.pt<pt_high)]
+    return TopsInSlice
+
+
 
 ################## HISTOGRAMS ##################
 """
@@ -263,5 +283,99 @@ if do_EfficiencyOverThrVsScoreThrs:
             if save_graphics:
                 histo.Write()
 
+
+"""
+Counts vs. Score in pt slices using:
+1. pt_limits = np.arange(start=200, stop=550, step=50)
+2. TopLowPt_pt in [0, 1000]GeV
+3. TopHighPt_pt in [50, inf]GeV
+"""
+if do_CountsVsScore_ptSlices:
+    nbins    = 20
+    xmin     = 0
+    xmax     = 1
+    pt_start = 0
+    pt_stop  = 1000
+    n_slices = 10
+    sls      = slices(pt_start=pt_start, pt_stop=pt_stop, n_slices=n_slices)
+    for pt_flag in pt_flags:
+        for truth in truths:
+            for s in sls:
+                scores         = []
+                if num_entries:
+                    Tops_In_Slices = TopsInSlice(tree=tree, pt_flag=pt_flag, truth=truth, pt_low=s[0], pt_high=s[1])
+                    if pt_flag=="Low":
+                        scores = ak.flatten(Tops_In_Slices.scoreDNN)
+                    elif pt_flag=="High":
+                        scores = ak.flatten(Tops_In_Slices.score2)
+                        
+                # Create and Fill TH1F 
+                histoName  = f"Top{pt_flag}Pt_CountsVsScore_{truth}_pt_{int(s[0])}_{int(s[1])}"
+                histoTitle = f"Top{pt_flag}Pt_CountsVsScore_{truth}_pt_{int(s[0])}_{int(s[1])}"
+                histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+                for score in scores:
+                    histo.Fill(score)
+                    
+                histo.GetXaxis().SetTitle("Score")
+                histo.GetYaxis().SetTitle("Counts")
+                histo.SetFillStyle(3001)
+                histo.SetLineWidth(2)
+                histo.SetLineColor(ROOT.kBlue)
+                # histo.Draw()
+                # Save histo to .root file 
+                if save_graphics:
+                    histo.Write()
+            
+
+"""
+Efficiency(Counts/totCounts) vs. Score using:
+1. pt_limits = np.arange(start=200, stop=550, step=50)
+2. TopLowPt_pt in [0, 1000]GeV
+3. TopHighPt_pt in [50, inf]GeV
+"""
+if do_EfficiencyVsScore_ptSlices:
+    nbins    = 20
+    xmin     = 0
+    xmax     = 1
+    pt_start = 0
+    pt_stop  = 1000
+    n_slices = 10
+    sls      = slices(pt_start=pt_start, pt_stop=pt_stop, n_slices=n_slices)
+    for pt_flag in pt_flags:
+        for truth in truths:
+            for s in sls:
+                scores         = []
+                if num_entries:
+                    Tops_In_Slices = TopsInSlice(tree=tree, pt_flag=pt_flag, truth=truth, pt_low=s[0], pt_high=s[1])
+                    if pt_flag=="Low":
+                        scores = ak.flatten(Tops_In_Slices.scoreDNN)
+                    elif pt_flag=="High":
+                        scores = ak.flatten(Tops_In_Slices.score2)
+                        
+                # Create and Fill TH1F 
+                histoName  = f"Top{pt_flag}Pt_EfficiencyVsScore_{truth}_pt_{int(s[0])}_{int(s[1])}"
+                histoTitle = f"Top{pt_flag}Pt_EfficiencyVsScore_{truth}_pt_{int(s[0])}_{int(s[1])}"
+                histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+                for score in scores:
+                    histo.Fill(score)
+                    
+                # Normalize to integral to obtain efficiency
+                if histo.Integral(0, nbins+1):
+                    histo.Scale(1/histo.Integral(0, nbins+1))
+                else:
+                    histo.Scale(1)
+                histo.GetXaxis().SetTitle("Score")
+                histo.GetYaxis().SetTitle("Efficiency")
+                histo.SetFillStyle(3001)
+                histo.SetLineWidth(2)
+                histo.SetLineColor(ROOT.kBlue)
+                # histo.Draw()
+                # Save histo to .root file 
+                if save_graphics:
+                    histo.Write()
+                    
+
+
+# Close file
 if save_graphics:
     PlotsRFile.Close()
